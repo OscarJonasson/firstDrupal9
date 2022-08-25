325f49a9ea22c27587bcbd3810baf9910d722977
Jonasson Oscar <oscjjonasson@gmail.com>
2022-08-25 14:31:56 +0300

    end of lesson

--- modules/custom/riddler_module/src/Controller/RiddlerController.php
+++ modules/custom/riddler_module/src/Controller/RiddlerController.php
@@ -1,11 +1,32 @@
 <?php
 
 namespace Drupal\riddler_module\Controller;
+// controllerbase out of nowhere??
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
-class RiddlerController{
+// use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+class RiddlerController extends ControllerBase {
 
     public function riddle(){
         // $random = rand(int 0, int 5);
-        return array('#markup'=> 'ASDFASFASDASFASDAFSDFASAFSAFSD');
+
+        // return array('#markup'=> $this->t("Welcome"));
+        return ['#markup' => $this->t('Haloo')];
     }
 }
+
+
+// This works too
+// class RiddlerController {
+
+//     use StringTranslationTrait;
+
+//     public function riddle(){
+//         // $random = rand(int 0, int 5);
+
+//         // return array('#markup'=> $this->t("Welcome"));
+//         return ['#markup' => $this->t('Haloo')];
+//     }
+// }

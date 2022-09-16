86439dc8bc151d9c0873a7eb744411b38a262170
Jonasson Oscar <oscjjonasson@gmail.com>
2022-09-16 11:59:13 +0300

    end of lesson auto generated custom modules

---
+++ themes/custom/custom_auto_theme/js/custom-auto-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Custom_auto_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.customAutoTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
